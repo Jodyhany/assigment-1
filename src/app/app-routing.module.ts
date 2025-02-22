a9c2190417aca5a52661dc0404b9a0f63108e8b7
Jodyhany <jody.hany.511@gmail.com>
2025-02-22 16:17:39 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ErrorComponent } from './error/error.component';
+import { workcomponents } from './work/work.component';
+import { ExperienceComponent } from './experoence/Experience.component';
+import { AppComponent } from './app.component';
+import { SoftwareComponent } from './software/software.component';
+import { AppsComponent } from './apps/apps.component';
+import { GraphicdesignComponent } from './graphicdesign/graphicdesign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HeroComponent, title: 'Home' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+  { path: 'Experience', component: ExperienceComponent, title: 'Experience' },
+  { path: 'skills', component: SkillsComponent, title: 'Skills' },
+  { path: 'work', component:workcomponents, title: 'Work'},
+  { path: '**', component: ErrorComponent, title: 'Not found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ErrorComponent } from './error/error.component';
+import { workcomponents } from './work/work.component';
+import { ExperienceComponent } from './experoence/Experience.component';
+import { FormsModule } from '@angular/forms';
+import { AppsComponent } from './apps/apps.component';
+import { SoftwareComponent } from './software/software.component';
+import { GraphicdesignComponent } from './graphicdesign/graphicdesign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    workcomponents,
+    ErrorComponent,
+    AppsComponent,
+    SoftwareComponent,
+    GraphicdesignComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule,FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
